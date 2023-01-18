a8f75d2cef0b3caf0fc67eb3963c5b9c8320035c
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-01-18 15:17:27 +0800

    add etch a sketch

--- script.js
+++ script.js
@@ -3,5 +3,27 @@ const cdiv = document.querySelector('.container');
 for(i=1;i<257;i++){
     const div = document.createElement('div');
     div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    div.id = `${i}`;
     cdiv.appendChild(div);
 }
+
+//hover
+//cdiv.addEventListener('click',(e)=>console.log(e.target.id));
+
+
+cdiv.addEventListener('mousemove',(e)=>{
+    e.preventDefault();
+    const gg = document.getElementById(`${e.target.id}`);
+    if (e.buttons == 1 ){
+        gg.style.background= 'black';
+    }
+});
+
+
+
+ 
+
+
+
+
+
