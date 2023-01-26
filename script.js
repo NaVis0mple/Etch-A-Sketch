7d19dee1b86818a975f753092c6b8143a5fbe420
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-01-26 23:59:32 +0800

    grid adjust

--- script.js
+++ script.js
@@ -2,29 +2,27 @@
 let cdiv = document.querySelector('.container');
 const box = document.querySelector('input');
 
-
-
-for(i=1;i<257;i++){
-    const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-    div.id = `${i}`;
-    cdiv.appendChild(div);
-}
-
 box.addEventListener('input',(e)=>{
     console.log(e.target.value);
     cdiv.replaceChildren();
-    for(i=1;i<e.target.value;i++){
+    for(i=0;i<Math.pow(e.target.value,2);i++){
         const div = document.createElement('div');
-        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-        div.id = `${i}`;
+        div.style.cssText = `border: 1px solid black; height: 25px; width: 25px`;
+        div.id = `${i+1}`;
         cdiv.appendChild(div);
+        cdiv.style.gridTemplateColumns = `repeat(${e.target.value}, 25px)`; 
     }
     
     }
 )
 
 
+
+let button= document.getElementById('but1');
+button.addEventListener('click',()=>{
+    cdiv.replaceChildren();
+    
+})
 
 
 cdiv.addEventListener('mousemove',(e)=>{
