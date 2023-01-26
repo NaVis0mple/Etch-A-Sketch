00fcf56f25c1d2abf7ee09e499f8c59cfba79115
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-01-26 15:41:07 +0800

    add bar

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //create div in html
-const cdiv = document.querySelector('.container');
+let cdiv = document.querySelector('.container');
+const box = document.querySelector('input');
+
+
+
 for(i=1;i<257;i++){
     const div = document.createElement('div');
     div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
@@ -7,8 +11,20 @@ for(i=1;i<257;i++){
     cdiv.appendChild(div);
 }
 
-//hover
-//cdiv.addEventListener('click',(e)=>console.log(e.target.id));
+box.addEventListener('input',(e)=>{
+    console.log(e.target.value);
+    cdiv.replaceChildren();
+    for(i=1;i<e.target.value;i++){
+        const div = document.createElement('div');
+        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        div.id = `${i}`;
+        cdiv.appendChild(div);
+    }
+    
+    }
+)
+
+
 
 
 cdiv.addEventListener('mousemove',(e)=>{
