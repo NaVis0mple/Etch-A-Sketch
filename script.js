fbceedd99517ff3187886f3f8742b72f29e8c511
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-01-29 14:29:33 +0800

    chatgpt can help

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-//create div in html
-let cdiv = document.querySelector('.container');
-const box = document.querySelector('input');
-
-box.addEventListener('input',(e)=>{
-    console.log(e.target.value);
-    cdiv.replaceChildren();
-    for(i=0;i<Math.pow(e.target.value,2);i++){
-        const div = document.createElement('div');
-        div.style.cssText = `border: 1px solid black; height: 25px; width: 25px`;
-        div.id = `${i+1}`;
-        cdiv.appendChild(div);
-        cdiv.style.gridTemplateColumns = `repeat(${e.target.value}, 25px)`; 
+//using chatgpt
+let gridContainer = document.getElementById("grid-container");
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 16; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener('mousedown', (e) => {
+            e.preventDefault();   //this prevent drag 
+            if (e.buttons === 1) {
+                cell.style.backgroundColor = 'black';
+            }
+        });
+        cell.addEventListener('mouseover',(e)=>{
+        
+            e.preventDefault();
+            if(e.buttons  == 1){
+                cell.style.backgroundColor = 'black';
+           }
+            
+        }
+        )
+        row.appendChild(cell);
     }
-    
-    }
-)
-
-
-
-let button= document.getElementById('but1');
-button.addEventListener('click',()=>{
-    cdiv.replaceChildren();
-    
-})
-
-
-cdiv.addEventListener('mousemove',(e)=>{
-    e.preventDefault();
-    const gg = document.getElementById(`${e.target.id}`);
-    if (e.buttons == 1 ){
-        gg.style.background= 'black';
-    }
-});
-
+    gridContainer.appendChild(row);
+}
 
 
  
